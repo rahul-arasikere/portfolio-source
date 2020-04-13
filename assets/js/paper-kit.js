aba51118407817b2a05858918fd51eb7ef6faadb
Rahul Arasikere <arasikere.rahul@gmail.com>
2020-04-13 19:38:13 +0530

    added education

--- assets/js/paper-kit.js
+++ assets/js/paper-kit.js
@@ -15,7 +15,6 @@
 
  */
 
-
 var transparent = true;
 var big_image;
 
@@ -26,7 +25,7 @@ var navbar_initialized,
   backgroundOrange = false,
   toggle_initialized = false;
 
-$(document).ready(function() {
+$(document).ready(function () {
   window_width = $(window).width();
   var big_image;
 
@@ -42,7 +41,7 @@ $(document).ready(function() {
 
   //  Append modals to <body>
   if ($(".modal").length != 0) {
-    $('.modal').appendTo('body');
+    $(".modal").appendTo("body");
   }
 
   // Init noUiSlider
@@ -55,208 +54,219 @@ $(document).ready(function() {
   pk.initNavbarImage();
 
   // Navbar color change on scroll
-  if ($('.navbar[color-on-scroll]').length != 0) {
-    $(window).on('scroll', pk.checkScrollForTransparentNavbar);
+  if ($(".navbar[color-on-scroll]").length != 0) {
+    $(window).on("scroll", pk.checkScrollForTransparentNavbar);
   }
 
-  $('.navbar-collapse').click(function() {
-    setTimeout(function() {
+  $(".navbar-collapse").click(function () {
+    setTimeout(function () {
       if (pk.misc.navbar_menu_visible == 1) {
-        $('html').removeClass('nav-open');
+        $("html").removeClass("nav-open");
         pk.misc.navbar_menu_visible = 0;
-        $('#bodyClick').remove();
-        setTimeout(function() {
-          $toggle.removeClass('toggled');
+        $("#bodyClick").remove();
+        setTimeout(function () {
+          $toggle.removeClass("toggled");
         }, 550);
       }
     }, 550);
   });
 
   // Change the collor of navbar collapse
-  $('#navbarToggler').on('show.bs.collapse', function() {
-    if ($('nav').hasClass('navbar-transparent') && $(document).scrollTop() < 50) {
-      $('.navbar').addClass('no-transition');
-      $('nav').removeClass('navbar-transparent');
-    }
-  }).on('hidden.bs.collapse', function() {
-    if ($(document).scrollTop() < 50) {
-      $('.navbar').removeClass('no-transition');
-      $('nav:first-of-type').addClass('navbar-transparent');
-    }
-  });
+  $("#navbarToggler")
+    .on("show.bs.collapse", function () {
+      if (
+        $("nav").hasClass("navbar-transparent") &&
+        $(document).scrollTop() < 50
+      ) {
+        $(".navbar").addClass("no-transition");
+        $("nav").removeClass("navbar-transparent");
+      }
+    })
+    .on("hidden.bs.collapse", function () {
+      if ($(document).scrollTop() < 50) {
+        $(".navbar").removeClass("no-transition");
+        $("nav:first-of-type").addClass("navbar-transparent");
+      }
+    });
 
-  $navbar = $('.navbar[color-on-scroll]');
-  scroll_distance = $navbar.attr('color-on-scroll') || 500;
+  $navbar = $(".navbar[color-on-scroll]");
+  scroll_distance = $navbar.attr("color-on-scroll") || 500;
 
   // Check if we have the class "navbar-color-on-scroll" then add the function to remove the class "navbar-transparent" so it will transform to a plain color.
 
-  if ($('.navbar[color-on-scroll]').length != 0) {
+  if ($(".navbar[color-on-scroll]").length != 0) {
     pk.checkScrollForTransparentNavbar();
-    $(window).on('scroll', pk.checkScroll)
+    $(window).on("scroll", pk.checkScroll);
   }
 
-  $('.form-control').on("focus", function() {
-    $(this).parent('.input-group').addClass("input-group-focus");
-  }).on("blur", function() {
-    $(this).parent(".input-group").removeClass("input-group-focus");
-  });
-
+  $(".form-control")
+    .on("focus", function () {
+      $(this).parent(".input-group").addClass("input-group-focus");
+    })
+    .on("blur", function () {
+      $(this).parent(".input-group").removeClass("input-group-focus");
+    });
 
   if (window_width >= 768) {
     big_image = $('.page-header[data-parallax="true"]');
 
     if (big_image.length != 0) {
-      $(window).on('scroll', pk.checkScrollForPresentationPage);
+      $(window).on("scroll", pk.checkScrollForPresentationPage);
     }
   }
   // Activate Carousel
-  $('.carousel').carousel({
-    interval: 4000
+  $(".carousel").carousel({
+    interval: 4000,
   });
-
 });
 
-$(document).on('click', '.navbar-toggler', function() {
+$(document).on("click", ".navbar-toggler", function () {
   $toggle = $(this);
 
   if (pk.misc.navbar_menu_visible == 1) {
-    $('html').removeClass('nav-open');
+    $("html").removeClass("nav-open");
     pk.misc.navbar_menu_visible = 0;
-    $('#bodyClick').remove();
-    setTimeout(function() {
-      $toggle.removeClass('toggled');
+    $("#bodyClick").remove();
+    setTimeout(function () {
+      $toggle.removeClass("toggled");
     }, 550);
   } else {
-    setTimeout(function() {
-      $toggle.addClass('toggled');
+    setTimeout(function () {
+      $toggle.addClass("toggled");
     }, 580);
     div = '<div id="bodyClick"></div>';
-    $(div).appendTo('body').click(function() {
-      $('html').removeClass('nav-open');
-      pk.misc.navbar_menu_visible = 0;
-      setTimeout(function() {
-        $toggle.removeClass('toggled');
-        $('#bodyClick').remove();
-      }, 550);
-    });
+    $(div)
+      .appendTo("body")
+      .click(function () {
+        $("html").removeClass("nav-open");
+        pk.misc.navbar_menu_visible = 0;
+        setTimeout(function () {
+          $toggle.removeClass("toggled");
+          $("#bodyClick").remove();
+        }, 550);
+      });
 
-    $('html').addClass('nav-open');
+    $("html").addClass("nav-open");
     pk.misc.navbar_menu_visible = 1;
   }
 });
 
 pk = {
   misc: {
-    navbar_menu_visible: 0
+    navbar_menu_visible: 0,
   },
 
-  checkScrollForTransparentNavbar: debounce(function() {
+  checkScrollForTransparentNavbar: debounce(function () {
     if ($(document).scrollTop() > $(".navbar").attr("color-on-scroll")) {
       if (transparent) {
         transparent = false;
-        $('.navbar[color-on-scroll]').removeClass('navbar-transparent');
+        $(".navbar[color-on-scroll]").removeClass("navbar-transparent");
       }
     } else {
       if (!transparent) {
         transparent = true;
-        $('.navbar[color-on-scroll]').addClass('navbar-transparent');
+        $(".navbar[color-on-scroll]").addClass("navbar-transparent");
       }
     }
   }, 17),
 
-  initNavbarImage: function() {
-    var $navbar = $('.navbar').find('.navbar-translate').siblings('.navbar-collapse');
-    var background_image = $navbar.data('nav-image');
+  initNavbarImage: function () {
+    var $navbar = $(".navbar")
+      .find(".navbar-translate")
+      .siblings(".navbar-collapse");
+    var background_image = $navbar.data("nav-image");
 
-    if ($(window).width() < 991 || $('body').hasClass('burger-menu')) {
+    if ($(window).width() < 991 || $("body").hasClass("burger-menu")) {
       if (background_image != undefined) {
-        $navbar.css('background', "url('" + background_image + "')")
-          .removeAttr('data-nav-image')
-          .css('background-size', "cover")
-          .addClass('has-image');
+        $navbar
+          .css("background", "url('" + background_image + "')")
+          .removeAttr("data-nav-image")
+          .css("background-size", "cover")
+          .addClass("has-image");
       }
     } else if (background_image != undefined) {
-      $navbar.css('background', "")
-        .attr('data-nav-image', '' + background_image + '')
-        .css('background-size', "")
-        .removeClass('has-image');
+      $navbar
+        .css("background", "")
+        .attr("data-nav-image", "" + background_image + "")
+        .css("background-size", "")
+        .removeClass("has-image");
     }
   },
 
-  initPopovers: function() {
+  initPopovers: function () {
     if ($('[data-toggle="popover"]').length != 0) {
-      $('body').append('<div class="popover-filter"></div>');
+      $("body").append('<div class="popover-filter"></div>');
 
       //    Activate Popovers
-      $('[data-toggle="popover"]').popover().on('show.bs.popover', function() {
-        $('.popover-filter').click(function() {
-          $(this).removeClass('in');
-          $('[data-toggle="popover"]').popover('hide');
+      $('[data-toggle="popover"]')
+        .popover()
+        .on("show.bs.popover", function () {
+          $(".popover-filter").click(function () {
+            $(this).removeClass("in");
+            $('[data-toggle="popover"]').popover("hide");
+          });
+          $(".popover-filter").addClass("in");
+        })
+        .on("hide.bs.popover", function () {
+          $(".popover-filter").removeClass("in");
         });
-        $('.popover-filter').addClass('in');
-      }).on('hide.bs.popover', function() {
-        $('.popover-filter').removeClass('in');
-      });
-
     }
   },
 
-  initSliders: function() {
+  initSliders: function () {
     // Sliders for demo purpose in refine cards section
-    if ($('#sliderRegular').length != 0) {
-      var rangeSlider = document.getElementById('sliderRegular');
+    if ($("#sliderRegular").length != 0) {
+      var rangeSlider = document.getElementById("sliderRegular");
       noUiSlider.create(rangeSlider, {
         start: [5000],
         range: {
-          'min': [2000],
-          'max': [10000]
-        }
+          min: [2000],
+          max: [10000],
+        },
       });
     }
-    if ($('#sliderDouble').length != 0) {
-      var slider = document.getElementById('sliderDouble');
+    if ($("#sliderDouble").length != 0) {
+      var slider = document.getElementById("sliderDouble");
       noUiSlider.create(slider, {
         start: [20, 80],
         connect: true,
         range: {
-          'min': 0,
-          'max': 100
-        }
+          min: 0,
+          max: 100,
+        },
       });
     }
-
   },
-
 
   // Javascript for the parallax
 
-  checkScroll: debounce(function() {
+  checkScroll: debounce(function () {
     big_image = $('.page-header[data-parallax="true"]');
-    oVal = ($(window).scrollTop() / 3);
+    oVal = $(window).scrollTop() / 3;
     big_image.css({
-      'transform': 'translate3d(0,' + oVal + 'px,0)',
-      '-webkit-transform': 'translate3d(0,' + oVal + 'px,0)',
-      '-ms-transform': 'translate3d(0,' + oVal + 'px,0)',
-      '-o-transform': 'translate3d(0,' + oVal + 'px,0)'
+      transform: "translate3d(0," + oVal + "px,0)",
+      "-webkit-transform": "translate3d(0," + oVal + "px,0)",
+      "-ms-transform": "translate3d(0," + oVal + "px,0)",
+      "-o-transform": "translate3d(0," + oVal + "px,0)",
     });
   }, 4),
-}
+};
 
 demo = {
-  initPickColor: function() {
-    $('.pick-class-label').click(function() {
-      var new_class = $(this).attr('new-class');
-      var old_class = $('#display-buttons').attr('data-class');
-      var display_div = $('#display-buttons');
+  initPickColor: function () {
+    $(".pick-class-label").click(function () {
+      var new_class = $(this).attr("new-class");
+      var old_class = $("#display-buttons").attr("data-class");
+      var display_div = $("#display-buttons");
       if (display_div.length) {
-        var display_buttons = display_div.find('.btn');
+        var display_buttons = display_div.find(".btn");
         display_buttons.removeClass(old_class);
         display_buttons.addClass(new_class);
-        display_div.attr('data-class', new_class);
+        display_div.attr("data-class", new_class);
       }
     });
-  }
-}
+  },
+};
 
 // Returns a function, that, as long as it continues to be invoked, will not
 // be triggered. The function will be called after it stops being called for
@@ -265,38 +275,37 @@ demo = {
 
 function debounce(func, wait, immediate) {
   var timeout;
-  return function() {
+  return function () {
     var context = this,
       args = arguments;
     clearTimeout(timeout);
-    timeout = setTimeout(function() {
+    timeout = setTimeout(function () {
       timeout = null;
       if (!immediate) func.apply(context, args);
     }, wait);
     if (immediate && !timeout) func.apply(context, args);
   };
-};
+}
 
 function hasScrolled() {
   var st = $(this).scrollTop();
   // Make sure they scroll more than delta
-  if (Math.abs(lastScrollTop - st) <= delta)
-    return;
+  if (Math.abs(lastScrollTop - st) <= delta) return;
 
   // If they scrolled down and are past the navbar, add class .nav-up.
   // This is necessary so you never see what is "behind" the navbar.
   if (st > lastScrollTop && st > navbarHeight) {
     // Scroll Down
-    $('.navbar.nav-down').removeClass('nav-down').addClass('nav-up');
+    $(".navbar.nav-down").removeClass("nav-down").addClass("nav-up");
   } else {
     // Scroll Up
     if (st + $(window).height() < $(document).height()) {
-      $('.navbar.nav-up').removeClass('nav-up').addClass('nav-down');
+      $(".navbar.nav-up").removeClass("nav-up").addClass("nav-down");
     }
   }
 
   lastScrollTop = st;
-};
+}
 
 // Returns a function, that, as long as it continues to be invoked, will not
 // be triggered. The function will be called after it stops being called for
@@ -305,14 +314,15 @@ function hasScrolled() {
 
 function debounce(func, wait, immediate) {
   var timeout;
-  return function() {
+  return function () {
     var context = this,
       args = arguments;
     clearTimeout(timeout);
-    timeout = setTimeout(function() {
+    timeout = setTimeout(function () {
       timeout = null;
       if (!immediate) func.apply(context, args);
     }, wait);
     if (immediate && !timeout) func.apply(context, args);
   };
-};
+}
+
